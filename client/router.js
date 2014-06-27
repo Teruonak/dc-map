f777376fc90e8e2b0bbd6dc6b629901ab66465c6
Marcel Teruo Nakamine <marcel.nakamine@outlook.com>
2014-06-27 02:43:16 -0300

    clean the project
    removed some libs was not used anymore.

--- client/router.js
+++ client/router.js
@@ -6,17 +6,5 @@ Router.map(function () {
   this.route('comingsoon', {path: '/'}),
   this.route('add'),
   this.route('touch'),
-  this.route('maps', {
-    path: '/maps',
-    onBeforeAction: function(pause){
-      var jvectormap = IRLibLoader.load('/client/lib/jvectormap/jquery-jvectormap-1.2.2.min.js', {
-      });
-      if(!jvectormap.ready()){ return pause(); }
-      var jvectormap1 = IRLibLoader.load('/client/lib/jvectormap/jquery-jvectormap-world-mill-en.js', {
-      });
-    },
-    waitOn: function(){
-        return [IRLibLoader.load(""), IRLibLoader.load("")]
-    }
-  });
+  this.route('maps')
 });
